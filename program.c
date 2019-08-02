4fe40005b51fb5f758d78299cff522f8eb5c3517
Jason Brugger <jason.brugger@gmail.com>
2019-08-02 13:46:54 -0700

    lesson math library completed

--- program.c
+++ program.c
@@ -1,9 +1,12 @@
+/*Write your C code here*/
 #include <stdio.h>
 #include <math.h>
 
 int main() {
-    int input;
-    int result = scanf("%d", &input);
-
-    printf("%0.8f", sqrt(input));
+	int input;
+	scanf("%d", &input);
+	
+	printf("%0.8f\n", sqrt(input));
+	printf("%0.10f\n", exp(1));
+	printf("gcc -std=c11 -Wall -fmax-errors=10 -Wextra program.c -lm -o program\n");
 }
